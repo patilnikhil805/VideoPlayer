c064bdd1919bd2c82e4fa13d215ba8602eb2df27
patilnikhil805 <pnikhil.805@gmail.com>
2025-03-15 21:37:59 -0400

    trpc configured

--- videoplayer/src/app/layout.tsx
+++ videoplayer/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
+import { TRPCProvider } from "@/trpc/client";
 
 
 const inter = Inter({
@@ -20,7 +21,9 @@ export default function RootLayout({
         <body
           className={inter.className}
           >
-          {children}
+            <TRPCProvider>
+              {children}
+            </TRPCProvider>
         </body>
       </html>
     </ClerkProvider>
