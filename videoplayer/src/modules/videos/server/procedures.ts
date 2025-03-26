dfae9ad2e8a284456d1f38032f6e97672267ed76
patilnikhil805 <pnikhil.805@gmail.com>
2025-03-25 22:13:52 -0400

    on mux integration 7:01:08

--- videoplayer/src/modules/videos/server/procedures.ts
+++ videoplayer/src/modules/videos/server/procedures.ts
@@ -4,20 +4,43 @@ import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { eq, and, or, lt, desc } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
+import { mux } from "@/lib/mux";
 
 export const videosRouter = createTRPCRouter({
     create: protectedProcedure.mutation(async ({ ctx, input }) => {
         const { id: userId } = ctx.user;
+
+        const upload = await mux.video.uploads.create({
+            new_asset_settings: {
+              passthrough: userId,
+              playback_policy: ["public"],
+              input: [
+                {
+                  generated_subtitles: [
+                    {
+                      language_code: "en",
+                      name: "English",
+                    },
+                  ],
+                },
+              ],
+            },
+            cors_origin: "*", // TODO: in production, this should be the domain of the app
+          });
 
         const [video] = await db
             .insert(videos)
             .values({
                 userId,
                 title: "Untitled",
+                muxStatus: "waiting",
+                muxUploadId: upload.id,
             })
             .returning();
         return {
             video: video,
+            url: upload.url,
+
         }
         
     })
